51fae1684cd8bef279caf5d9313108021b58f069
João Fonseca <joao.fonseca@ua.pt>
2024-06-07 20:28:17 +0100

    add logger

--- src/what_the_music.cpp
+++ src/what_the_music.cpp
@@ -4,6 +4,7 @@
 #include "engine/database.h"
 #include "util/file_reader.h"
 #include "util/program_arguments.h"
+#include "util/csv_logger.h"
 
 using namespace std;
 using namespace std::filesystem;
@@ -23,6 +24,12 @@ int main(int argc, char *argv[]) {
     Database database = Database(args.databaseFolder, compressor);
     database.load();
 
+    // instantiate logger
+    CSVLogger logger;
+    if (!args.logFilePath.empty()) {
+        logger = CSVLogger(args.logFilePath);
+    }
+
     // query database
     for (const auto &entry: directory_iterator(args.queriesFolder)) {
         // get path not the string
@@ -30,17 +37,29 @@ int main(int argc, char *argv[]) {
         if (filePath.has_extension() && filePath.extension().string() != ".freqs") {
             continue;
         }
+
         // read signature from file
         FileReader fileReader = FileReader(filePath.string());
         fileReader.read();
         string signature = fileReader.getContent();
+
         // query database
         cout << "Querying " << filePath.stem().string() << "..." << endl;
         vector<tuple<basic_string<char>, double>> result = database.query(signature);
-        // print results
-        cout << "Results" << endl;
-        for (const auto &entry: result) {
-            cout << "   " << get<0>(entry) << " (" << get<1>(entry) << ")" << endl;
+
+        // print or log results
+        if (args.logFilePath.empty()) {
+            cout << "Results" << endl;
+            for (const auto &entry: result) {
+                cout << "   " << get<0>(entry) << " (" << get<1>(entry) << ")" << endl;
+            }
+        } else {
+            vector<string> output = {filePath.stem().string()}; // query file name
+            for (const auto &entry: result) {
+                output.push_back(get<0>(entry)); // music file name
+                output.push_back(to_string(get<1>(entry))); // similarity
+            }
+            logger.append(output);
         }
     }
 
